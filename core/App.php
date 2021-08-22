5b471a766e89a5ea74e8c2286ea5679e62a7972a
Nikita Nikolaev <nn.efymovich@gmail.com>
2021-08-22 12:43:11 +0300

    refactoring

--- core/App.php
+++ core/App.php
@@ -8,10 +8,6 @@ use core\Router\RouteContainer;
 require "database.php";
 require "helpers.php";
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
 class App
 {
     public static function Init()
